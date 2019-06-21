9f729ec44ef3b33e2b6e68f6a47f4350bf17906f
adsalor <adsalor@gmail.com>
2019-06-21 19:56:08 -0400

    Added instance handles for WM_CLOSE and WM_PAINT to WindowProc Added Message Loop

--- openGL/opengl render engine/main.cpp
+++ openGL/opengl render engine/main.cpp
@@ -5,6 +5,21 @@
 #include <iostream>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	if (uMsg == WM_DESTROY) {
+		PostQuitMessage(0);
+		return 0;
+	}
+	if (uMsg == WM_CLOSE) {
+		DestroyWindow(hwnd);
+	}
+	if (uMsg == WM_PAINT) {
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+
+		
+		FillRect(hdc, &ps.rcPaint, CreateSolidBrush(RGB(128,128,128)));
+		EndPaint(hwnd, &ps);
+	}
 	return (DefWindowProc(hwnd, uMsg, wParam, lParam));
 };
 
@@ -35,13 +50,15 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 		hInstance,  // Instance handle
 		NULL        // Additional application data
 	);
-	while (true) {
-		if (hwnd == NULL)
-		{
-			std::cin.get();
-			return 0;
-		}
-
-		ShowWindow(hwnd, nCmdShow);
+	if (hwnd == NULL) {
+		return 0;
 	}
+	ShowWindow(hwnd, nCmdShow);
+	MSG msg = { };
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return 0;
 }
